2cf1ecc79e73397c735a54b11be713f7d17b7c85
ZholobovSS <zholobovsemyonsergeevich@gmail.com>
2016-12-13 21:42:41 +0300

    Add Server

---
+++ ChessServer/ChessServer/ClientObject.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChessServer
+{
+    class ClientObject
+    {
+        protected internal string Id { get; private set; }
+        string username;
+        TcpClient client;
+        ServerObject server;
+        string serverMessage;
+        Random rnd = new Random();
+        string WhoIam;
+        protected internal NetworkStream Stream { get; private set; }
+
+
+        public ClientObject (TcpClient tcpClient, ServerObject serverObject, int Counter)
+        {
+            Id = Counter.ToString();
+            client = tcpClient;
+            if (Counter % 2 == 0)
+                WhoIam = "w";
+            else
+                WhoIam = "b";
+            server = serverObject;
+            serverObject.AddConnection(this);
+        }
+        public void Process()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                string message = GetMessage();
+                username = message;
+
+                message = WhoIam;
+                server.BroadcastMessage(message, this.Id);
+                Console.WriteLine("{0}: присоединился к игре!", username);
+
+                while (true)
+                    try
+                    {
+                        message = GetMessage();
+                        message = String.Format("{0}{1}", WhoIam, message);
+                        serverMessage = String.Format("{0}: {1}", username, message);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                    }
+                    catch (Exception ex)
+                    {
+                        message = String.Format("{0}: покинул игру", username);
+                        Console.WriteLine(message);
+                        break;
+                    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private string GetMessage()
+        {
+            byte[] data = new byte[client.ReceiveBufferSize];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            } while (Stream.DataAvailable);
+            return builder.ToString(); 
+        }
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+}
---
+++ ChessServer/ChessServer/serverObject.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChessServer
+{
+    class ServerObject
+    {
+        private int Counter = 0;
+        static TcpListener tcpListener;
+        List<ClientObject> clients = new List<ClientObject>();
+
+        protected internal void AddConnection(ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+            Counter = clients.Count;
+        }
+        protected internal void RemoveConnection(string id)
+        {
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            if (client != null)
+                clients.Remove(client);
+        }
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание игроков...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    ClientObject clientObject = new ClientObject(tcpClient, this, Counter);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+        protected internal void BroadcastMessage(string message, string id)
+        {
+
+            if (message == "w" || message == "b")
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                for (int i = 0; i < clients.Count; i++)
+                {
+                    clients[i].Stream.Write(data, 0, data.Length);
+                }
+            }
+            else
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                for (int i = 0; i < clients.Count; i++)
+                {
+                    if (clients[i].Id != id)
+                        clients[i].Stream.Write(data, 0, data.Length);
+
+                }
+            }
+        }
+
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop();
+            for (int i = 0; i < clients.Count; i++)
+                clients[i].Close();
+            Environment.Exit(0);
+        }
+    }
+}
